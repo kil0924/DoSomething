f37c2cfe1f34312602963e16a8dfce356f729812
kil0924 <kil0924@gmail.com>
2025-05-14 23:47:24 +0900

    재부팅하니까 되네....

--- Assets/00. Core/Scripts/Build/Editor/BuildScript.cs
+++ Assets/00. Core/Scripts/Build/Editor/BuildScript.cs
@@ -1,10 +1,14 @@
 using UnityEditor;
 using System;
+using System.IO;
+using UnityEngine;
 
 public class BuildScript
 {
     public static void Build() 
     {
+        Console.WriteLine("빌드 시작");
+        
         PlayerSettings.Android.keystorePass = Environment.GetEnvironmentVariable("KEYSTORE_PASS");
         PlayerSettings.Android.keyaliasPass = Environment.GetEnvironmentVariable("KEYALIAS_PASS");
 
@@ -13,5 +17,6 @@ public class BuildScript
             buildPath, 
             BuildTarget.Android, 
             BuildOptions.None);
+        Console.WriteLine("빌드 끝");
     }
 }
