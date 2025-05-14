70794db0cb8ca6a87d050045cd8b9063199fbd1a
kil0924 <kil0924@gmail.com>
2025-05-14 22:12:06 +0900

    빌드스크립트 수정

--- Assets/00. Core/Scripts/Build/Editor/BuildScript.cs
+++ Assets/00. Core/Scripts/Build/Editor/BuildScript.cs
@@ -5,6 +5,9 @@ public class BuildScript
 {
     public static void Build() 
     {
+        PlayerSettings.Android.keystorePass = Environment.GetEnvironmentVariable("KEYSTORE_PASS");
+        PlayerSettings.Android.keyaliasPass = Environment.GetEnvironmentVariable("KEYALIAS_PASS");
+
         string buildPath = "BuildOutput/RPG.apk";
         BuildPipeline.BuildPlayer(new[] { "Assets/03. Rpg/Rpg.unity" }, 
             buildPath, 
